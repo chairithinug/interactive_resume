75bd659829a878fa1879938273e993a2f39599f0
Anapat Chairithinugull <chairithinug@uwalumni.com>
2025-10-03 21:21:02 +0200

    tried to add nav, didnt work

--- script.js
+++ script.js
@@ -379,3 +379,10 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+// const toggleBtn = document.getElementById('sidebar-toggle');
+//     const sidebar = document.getElementById('sidebar');
+
+//     toggleBtn.addEventListener('click', () => {
+//         sidebar.classList.toggle('-translate-x-full');
+//     });
